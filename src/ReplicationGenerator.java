fc967a61f9ca77d17826cdcbf01f46fe38f49a50
kljp <slashxp@naver.com>
2019-11-15 00:27:53 +0900

    #13 minor fix

--- src/ReplicationGenerator.java
+++ src/ReplicationGenerator.java
@@ -83,6 +83,8 @@ public class ReplicationGenerator {
         for (int i = 0; i < lsoArray.length; i++)
             loads[i] = lsoArray[i].getNumSubscriptions() * lsoArray[i].getAccessCount();
 
+        // currently, it is implemented by the least-loaded broker selection with considering both the number of subscription and the access count.
+        // In the future, more options should be additionally implemented: 1. random, 2. probabilistic, 3. only considering the number of subscription + every strategy.
         for (int i = 0; i < loads.length; i++) {
 
             for (int j = 0; j < loads.length - i - 1; j++) {
