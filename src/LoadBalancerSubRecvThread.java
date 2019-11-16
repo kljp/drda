5a3270f81069d73ca16f8a71241bfe022e0fb9f6
kljp <slashxp@naver.com>
2019-11-17 03:39:40 +0900

    #49

--- src/LoadBalancerSubRecvThread.java
+++ src/LoadBalancerSubRecvThread.java
@@ -57,8 +57,8 @@ public class LoadBalancerSubRecvThread extends Thread {
 
             if(messages.length > 1)
                 messages = replicationGenerator.preventDuplicates(messages, IPMap);
-
-            if(messages.length > repDeg.getRepDegInt() && lsos != null)
+            System.out.println(lsos.size());
+            if(messages.length > repDeg.getRepDegInt())
                 messages = replicationGenerator.applyReplicationDegree(messages, IPMap, lsos, repDeg.getRepDegInt());
 
             for (int i = 0; i < messages.length; i++) {
