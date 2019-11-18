3a7cb97ad270824b2f63e52b05ebc48d71dacdd9
kljp <slashxp@naver.com>
2019-11-18 16:23:34 +0900

    #133 segment degree: 2 -> 4

--- src/GlobalState.java
+++ src/GlobalState.java
@@ -7,7 +7,7 @@ public class GlobalState {
     public static final double[] maximumBounds = new double[]{180, 180, 180, 180, 180, 180, 180, 180};
     public static int UnderThresholdOfRange = 90;
     public static int OverThresholdOfRange = 180;
-    public static int NumberOfSegmentsPerDimension = 2; // segment degree
+    public static int NumberOfSegmentsPerDimension = 4; // segment degree
     public static final String[] segmentIdentifier = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}; // Currently, we support totally 15 identifiers except zero.
     public static int NumberOfDimensionGroups = 1;
     public static int NumberOfDimensionsPerGroup = NumberOfDimensions / NumberOfDimensionGroups;
@@ -22,7 +22,7 @@ public class GlobalState {
     public static int IPS_LB_PORT = 5007;
     public static int IPS_BROKER_PORT = 5008;
     public static int IPS_CLIENT_PORT = 5009;
-    public static int PUB_COUNT = 500;
+    public static int PUB_COUNT = 10000;
     public static double PeriodOfSync = 10.0;
     public static double REP_DEG_INIT = 3.0;
     public static int MAX_NUM_BROKER = 1024;
