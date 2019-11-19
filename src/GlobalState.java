c4313850f4a5bbd4c74bf9f4bc29a2ea9938e2fe
kljp <slashxp@naver.com>
2019-11-19 17:27:53 +0900

    #152

--- src/GlobalState.java
+++ src/GlobalState.java
@@ -5,8 +5,8 @@ public class GlobalState {
     public static String[] attributes2 = new String[]{"attr_0", "attr_1", "attr_2", "attr_3", "attr_4", "attr_5", "attr_6", "attr_7"}; // This is the order of the attributes for test.
     public static final double[] minimumBounds = new double[]{0, 0, 0, 0, 0, 0, 0, 0};
     public static final double[] maximumBounds = new double[]{180, 180, 180, 180, 180, 180, 180, 180};
-    public static int UnderThresholdOfRange = 0;
-    public static int OverThresholdOfRange = 180;
+    public static int UnderThresholdOfRange = 75;
+    public static int OverThresholdOfRange = 105;
     public static int NumberOfSegmentsPerDimension = 8; // segment degree
     public static final String[] segmentIdentifier = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}; // Currently, we support totally 15 identifiers except zero.
     public static int NumberOfDimensionGroups = 1;
