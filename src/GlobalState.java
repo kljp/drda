e722853ff74e2aec3cfcf534b67a96fc23fc4a2e
kljp <slashxp@naver.com>
2019-11-22 18:01:10 +0900

    #167

--- src/GlobalState.java
+++ src/GlobalState.java
@@ -5,9 +5,9 @@ public class GlobalState {
     public static String[] attributes2 = new String[]{"attr_0", "attr_1", "attr_2", "attr_3", "attr_4", "attr_5", "attr_6", "attr_7"}; // This is the order of the attributes for test.
     public static final double[] minimumBounds = new double[]{0, 0, 0, 0, 0, 0, 0, 0};
     public static final double[] maximumBounds = new double[]{180, 180, 180, 180, 180, 180, 180, 180};
-    public static int UnderThresholdOfRange = 75;
-    public static int OverThresholdOfRange = 105;
-    public static int NumberOfSegmentsPerDimension = 8; // segment degree
+    public static int UnderThresholdOfRange = 0;
+    public static int OverThresholdOfRange = 180;
+    public static int NumberOfSegmentsPerDimension = 2; // segment degree
     public static final String[] segmentIdentifier = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}; // Currently, we support totally 15 identifiers except zero.
     public static int NumberOfDimensionGroups = 1;
     public static int NumberOfDimensionsPerGroup = NumberOfDimensions / NumberOfDimensionGroups;
@@ -28,11 +28,11 @@ public class GlobalState {
     public static double REP_DEG_INIT = 3.0;
     public static int MAX_NUM_BROKER = 1024;
     public static int PERIOD_SYNC_START = 3;
-    public static int PERIOD_SYNC_END = 20;
+    public static int PERIOD_SYNC_END = 8;
     public static String EXP_MODE = "ON"; // "ON" or "OFF"
     public static String SKEWED_SUBSCRIPTION_MODE = "OFF"; // "ON" or "OFF"
     public static String SKEWED_PUBLICATION_MODE = "OFF"; // "ON" or "OFF"
     public static String LOAD_OPTION = "AC"; // "SUB": only consider the number of subscriptions, "AC": only consider the number of access counts, "ALL": consider both the number of subscriptions and the number of access counts.
     public static String DRDA_MODE = "OFF"; // "ON", "SEMI" or "OFF". SEMI: replication degree is always 3.
-    public static String UNSUB_MODE = "OFF"; // "ON" or "OFF"
+    public static String UNSUB_MODE = "ON"; // "ON" or "OFF"
 }
