b9c34f9ce8419ab5abb5646f93f2d927a0ddcb3d
kljp <slashxp@naver.com>
2019-11-25 16:46:37 +0900

    #183 seg=2, skewed=off, load=sub drda=on

--- src/ClientSubMain.java
+++ src/ClientSubMain.java
@@ -41,7 +41,7 @@ public class ClientSubMain {
             }
 
             try {
-                Thread.sleep(20);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
