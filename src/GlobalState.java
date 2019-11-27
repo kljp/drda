336b494e99340c8a6a0745077761e96bb219900e
kljp <slashxp@naver.com>
2019-11-27 16:19:57 +0900

    #230

--- src/GlobalState.java
+++ src/GlobalState.java
@@ -22,18 +22,18 @@ public class GlobalState {
     public static int IPS_LB_PORT = 5007;
     public static int IPS_BROKER_PORT = 5008;
     public static int IPS_CLIENT_PORT = 5009;
-    public static int SUB_COUNT = 800;
+    public static int SUB_COUNT = 600;
     public static int PUB_COUNT = 10000;
     public static double PeriodOfSync = 7.0;
     public static double REP_DEG_INIT = 3.0;
     public static int MAX_NUM_BROKER = 1024;
     public static int PERIOD_SYNC_START = 5;
     public static int PERIOD_SYNC_END = 15;
-    public static int SUB_TERM = 100;
+    public static int SUB_TERM = 150;
     public static String EXP_MODE = "ON"; // "ON" or "OFF"
     public static String SKEWED_SUBSCRIPTION_MODE = "OFF"; // "ON" or "OFF"
     public static String SKEWED_PUBLICATION_MODE = "OFF"; // "ON" or "OFF"
-    public static String LOAD_OPTION = "ALL"; // "SUB": only consider the number of subscriptions, "AC": only consider the number of access counts, "ALL": consider both the number of subscriptions and the number of access counts.
+    public static String LOAD_OPTION = "AC"; // "SUB": only consider the number of subscriptions, "AC": only consider the number of access counts, "ALL": consider both the number of subscriptions and the number of access counts.
     public static String DRDA_MODE = "ON"; // "ON", "SEMI" or "OFF". SEMI: replication degree is always 3.
     public static String UNSUB_MODE = "OFF"; // "ON" or "OFF"
 }
