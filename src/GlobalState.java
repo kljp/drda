cf5e4747961892f9f0245371d9db587eca998a76
kljp <slashxp@naver.com>
2019-11-27 14:54:15 +0900

    #221

--- src/GlobalState.java
+++ src/GlobalState.java
@@ -29,7 +29,7 @@ public class GlobalState {
     public static int MAX_NUM_BROKER = 1024;
     public static int PERIOD_SYNC_START = 5;
     public static int PERIOD_SYNC_END = 15;
-    public static int SUB_TERM = 50;
+    public static int SUB_TERM = 1000;
     public static String EXP_MODE = "ON"; // "ON" or "OFF"
     public static String SKEWED_SUBSCRIPTION_MODE = "OFF"; // "ON" or "OFF"
     public static String SKEWED_PUBLICATION_MODE = "OFF"; // "ON" or "OFF"
