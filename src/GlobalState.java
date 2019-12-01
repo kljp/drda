97d75956470a9cbee5e0f411b704e103895377ca
kljp <slashxp@naver.com>
2019-12-01 20:21:55 +0900

    #374

--- src/GlobalState.java
+++ src/GlobalState.java
@@ -7,7 +7,7 @@ public class GlobalState {
     public static final double[] maximumBounds = new double[]{180, 180, 180, 180, 180, 180, 180, 180};
     public static int UnderThresholdOfRange = 0;
     public static int OverThresholdOfRange = 180;
-    public static int NumberOfSegmentsPerDimension = 4; // segment degree
+    public static int NumberOfSegmentsPerDimension = 2; // segment degree
     public static final String[] segmentIdentifier = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}; // Currently, we support totally 15 identifiers except zero.
     public static int NumberOfDimensionGroups = 1;
     public static int NumberOfDimensionsPerGroup = NumberOfDimensions / NumberOfDimensionGroups;
@@ -32,10 +32,10 @@ public class GlobalState {
     public static int PERIOD_SYNC_END = 50;
     public static int SUB_TERM = 110;
     public static String EXP_MODE = "ON"; // "ON" or "OFF"
-    public static String SKEWED_SUBSCRIPTION_MODE = "OFF"; // "ON" or "OFF"
-    public static String SKEWED_PUBLICATION_MODE = "OFF"; // "ON" or "OFF"
+    public static String SKEWED_SUBSCRIPTION_MODE = "ON"; // "ON" or "OFF"
+    public static String SKEWED_PUBLICATION_MODE = "ON"; // "ON" or "OFF"
 //    public static String LOAD_OPTION = "ALL"; // "SUB": only consider the number of subscriptions, "AC": only consider the number of access counts, "ALL": consider both the number of subscriptions and the number of access counts.
-    public static String DRDA_MODE = "ON"; // "ON", "SEMI" or "OFF". SEMI: replication degree is always 3.
-    public static String DIST_MODE = "PBSUB"; // "LFSUB", "LFAC", "LFALL", "RAND", "PBSUB", "PBAC" or "PBALL"
+    public static String DRDA_MODE = "OFF"; // "ON", "SEMI" or "OFF". SEMI: replication degree is always 3.
+    public static String DIST_MODE = "LFSUB"; // "LFSUB", "LFAC", "LFALL", "RAND", "PBSUB", "PBAC" or "PBALL"
     public static String UNSUB_MODE = "OFF"; // "ON" or "OFF"
 }
